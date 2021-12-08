bf6d3bb2050e5a19aba11f83e658437b79c76834
princessbobsanta <princessbobsantaclaus@gmail.com>
2021-12-07 15:45:54 -1000

    leaflet image export workarounds:

--- src/app/components/leaflet-controls/leaflet-image-export/leaflet-image-export.component.ts
+++ src/app/components/leaflet-controls/leaflet-image-export/leaflet-image-export.component.ts
@@ -11,7 +11,9 @@ export class LeafletImageExportComponent implements OnInit {
   @ViewChild("exportControl") exportControl: ElementRef;
 
   @Input() position: ControlPosition = "topleft";
+  private _map: LMap;
   @Input() set map(map: LMap) {
+    this._map = map;
     if(map) {
       let ExportControl = Control.extend({
         onAdd: function () {
@@ -32,10 +34,20 @@ export class LeafletImageExportComponent implements OnInit {
 
   }
 
-  exportImage() {
+  async exportImage() {
+    this._map.dragging.disable();
+    this._map.touchZoom.disable();
+    this._map.doubleClickZoom.disable();
+    this._map.scrollWheelZoom.disable();
+    this._map.boxZoom.disable();
+    this._map.keyboard.disable();
+    if(this._map.tap) {
+      this._map.tap.disable();
+    }
+
     let canvas = document.createElement("canvas");
     let containerEl: HTMLElement = this.imageContainer.nativeElement;
-    let nodeReplacements: HTMLNodeReplaceData[] = this.replaceCanvasNodesWithImg(containerEl);
+    let nodeReplacements: HTMLNodeReplaceData[] = await this.prepareDOMForExport(containerEl);
     let mapBounds: DOMRect = containerEl.getBoundingClientRect();
     canvas.width = document.body.clientWidth;
     canvas.height = document.body.clientHeight;
@@ -56,8 +68,17 @@ export class LeafletImageExportComponent implements OnInit {
         data[0].style.display = data[1];
       }
       //revert converted image nodes to canvas
-      console.log(nodeReplacements);
       this.revertNodeReplacments(nodeReplacements);
+
+      this._map.dragging.enable();
+      this._map.touchZoom.enable();
+      this._map.doubleClickZoom.enable();
+      this._map.scrollWheelZoom.enable();
+      this._map.boxZoom.enable();
+      this._map.keyboard.enable();
+      if(this._map.tap) {
+        this._map.tap.enable();
+      }
     });
 
     for(let className of this.hiddenControls) {
@@ -78,45 +99,101 @@ export class LeafletImageExportComponent implements OnInit {
     }
   }
 
-  private replaceCanvasNodesWithImg(root: HTMLElement): HTMLNodeReplaceData[] {
-    let replaceData: HTMLNodeReplaceData[] = [];
-    let replaceItem: HTMLNodeReplaceData = {
-      root: root,
-      replacements: []
-    };
-    //removing child nodes in forEach causes issues
-    for(let child of Array.from(root.childNodes)) {
-      let node = <HTMLElement>child;
-      if(node.tagName == "CANVAS") {
-        let original = root.removeChild(node);
-        let canvasNode: HTMLCanvasElement = <HTMLCanvasElement>node;
-        let dataURL = canvasNode.toDataURL("2d");
-        let imageEl = document.createElement("img");
-        imageEl.src = dataURL;
-        //set html props
-        imageEl.className = canvasNode.className;
-        imageEl.width = canvasNode.width;
-        for(let style in canvasNode.style) {
-          try {
-            imageEl.style[style] = canvasNode.style[style];
-          }
-          catch {}
-        }
-        //append image node to root
-        root.appendChild(imageEl);
-        replaceItem.replacements.push({
-          original,
-          replacement: imageEl
-        });
-      }
-      replaceData = replaceData.concat(this.replaceCanvasNodesWithImg(node));
-    }
-    //only add the node if any replacements were made
-    if(replaceItem.replacements.length > 0) {
-      replaceData.push(replaceItem);
-    }
+  //cannot render canvases
+  private async prepareDOMForExport(root: HTMLElement): Promise<HTMLNodeReplaceData[]> {
+    return new Promise(async (resolve) => {
+      let replaceData: HTMLNodeReplaceData[] = [];
+      let replaceItem: HTMLNodeReplaceData = {
+        root: root,
+        replacements: []
+      };
+      //removing child nodes in forEach causes issues
+      for(let child of Array.from(root.childNodes)) {
+        let node = <HTMLElement>child;
+        if(node.tagName == "IMG") {
+          root.removeChild(node);
+          let imageNode: HTMLImageElement = <HTMLImageElement>node;
 
-    return replaceData;
+          let sourceClone: HTMLImageElement = new Image;
+          sourceClone.src = imageNode.src;
+          sourceClone.crossOrigin = "Anonymous";
+  
+          let canvasNode = document.createElement("canvas");
+          let ctx = canvasNode.getContext("2d");
+          canvasNode.width = imageNode.width;
+          canvasNode.height = imageNode.height;
+          
+          let imageEl = document.createElement("img");
+  
+          let imageDrawn = new Promise<void>((resolve) => {
+            let drawImageToContext = () => {
+              ctx.drawImage(sourceClone, 0, 0);
+              let dataURL = canvasNode.toDataURL();
+              imageEl.src = dataURL;
+              resolve();
+            }
+    
+            sourceClone.addEventListener("load", drawImageToContext);
+          });
+  
+          canvasNode.className = imageNode.className;
+          for(let style in imageNode.style) {
+            try {
+              canvasNode.style[style] = imageNode.style[style];
+            }
+            catch {}
+          }
+  
+          //set html props
+          imageEl.className = imageNode.className;
+          imageEl.width = imageNode.width;
+          for(let style in imageNode.style) {
+            try {
+              imageEl.style[style] = imageNode.style[style];
+            }
+            catch {}
+          }
+          //append image node to root
+          root.appendChild(imageEl);
+          replaceItem.replacements.push({
+            original: imageNode,
+            replacement: imageEl
+          });
+  
+          await imageDrawn;
+        }
+        if(node.tagName == "CANVAS") {
+          root.removeChild(node);
+          let canvasNode: HTMLCanvasElement = <HTMLCanvasElement>node;
+          let dataURL = canvasNode.toDataURL("2d");
+          let imageEl = document.createElement("img");
+          imageEl.src = dataURL;
+          //set html props
+          imageEl.className = canvasNode.className;
+          imageEl.width = canvasNode.width;
+          for(let style in canvasNode.style) {
+            try {
+              imageEl.style[style] = canvasNode.style[style];
+            }
+            catch {}
+          }
+          //append image node to root
+          root.appendChild(imageEl);
+          replaceItem.replacements.push({
+            original: canvasNode,
+            replacement: imageEl
+          });
+        }
+        replaceData = replaceData.concat(await this.prepareDOMForExport(node));
+      }
+      //only add the node if any replacements were made
+      if(replaceItem.replacements.length > 0) {
+        replaceData.push(replaceItem);
+      }
+  
+      resolve(replaceData);
+    });
+    
   }
 
 }
